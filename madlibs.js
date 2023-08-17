bfdf7fa145bdda75e45dadba96f60bb6200fe8c9
takidilmi <angeldilmi@gmail.com>
2023-08-17 21:01:24 +0200

    added reset button

--- madlibs.js
+++ madlibs.js
@@ -26,8 +26,8 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
+// Function to parse the raw story text
 function parseStory(rawStory) {
-  // Your code here.
   const array = [];
   const splittedText = rawStory.split(" ");
 
@@ -36,48 +36,39 @@ function parseStory(rawStory) {
       array.push({
         word: word.replace("[n]", ""),
         pos: "n"
-      })
+      });
     } else if ((/\[a\]/).test(word) === true) {
       array.push({
         word: word.replace("[a]", ""),
         pos: "a"
-      })
+      });
     } else if ((/\[v\]/).test(word) === true) {
       array.push({
         word: word.replace("[v]", ""),
         pos: "v"
-      })
+      });
     } else if ((/\[break\]/).test(word) === true) {
       array.push({
         word: word.replace("[break]", ""),
         pos: "break"
-      })} else {
+      });
+    } else {
       array.push({
         word: word
-      })
+      });
     }
   }
-  return array // This line is currently wrong :)
+  return array;
 }
-
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- * 
- * You'll want to use the results of parseStory() to display the story on the page.
- */
-/*getRawStory().then(parseStory).then((processedStory) => {
-  console.log(processedStory);
-}); */
-
-// Get a reference to the edit view and preview view elements
-const madLibsEdit = document.querySelector('.madLibsEdit');
-const madLibsPreview = document.querySelector('.madLibsPreview');
 
 // Get the processed story
 getRawStory().then(parseStory).then((processedStory) => {
   // Create an array to store input values
-  const inputValues = new Array(processedStory.length);
+  const inputValues = new Array(processedStory.length).fill('');
+
+  // Get a reference to the edit view and preview view elements
+  const madLibsEdit = document.querySelector('.madLibsEdit');
+  const madLibsPreview = document.querySelector('.madLibsPreview');
 
   // Function to render the edit view and the preview view
   function renderStory() {
@@ -86,70 +77,65 @@ getRawStory().then(parseStory).then((processedStory) => {
 
     for (let i = 0; i < processedStory.length; i++) {
       const wordObj = processedStory[i];
+
       if (wordObj.pos === 'break') {
-        madLibsEdit.appendChild(document.createElement("br")); // Add space
-        
-      }else if (wordObj.pos) {
-        // Create an input element for edit view
+        madLibsEdit.appendChild(document.createElement("br"));
+      } else if (wordObj.pos) {
         const input = document.createElement('input');
         input.type = 'text';
-        input.maxLength = 20; // Maximum of 20 characters
-        input.placeholder = wordObj.pos
+        input.maxLength = 20;
+        input.placeholder = wordObj.pos;
 
-        // Listen for input changes
+        input.value = inputValues[i] || '';
+
         input.addEventListener('input', (event) => {
-          // Update the input value array
           inputValues[i] = event.target.value;
-
-          // Render the preview view
           renderPreview();
         });
 
-        // Wrap the input in a span
         const inputSpan = document.createElement('span');
         inputSpan.appendChild(input);
 
-        // Append the input span to the edit view
         madLibsEdit.appendChild(inputSpan);
       } else {
-        // Wrap the word in a span
         const wordSpan = document.createElement('span');
         wordSpan.textContent = wordObj.word + ' ';
-
-        // Append the word span to the edit view
         madLibsEdit.appendChild(wordSpan);
       }
 
-      // Render the preview view
       renderPreview();
     }
   }
 
-  // Function to render the preview view
   function renderPreview() {
-    madLibsPreview.innerHTML = ''; // Clear previous content
+    madLibsPreview.innerHTML = '';
 
     for (let i = 0; i < processedStory.length; i++) {
       const wordObj = processedStory[i];
 
-      // Wrap each word in a span
       const wordSpan = document.createElement('span');
       if (wordObj.pos === 'break') {
         madLibsPreview.appendChild(document.createElement("br"));
-      }else if (wordObj.pos) {
+      } else if (wordObj.pos) {
         wordSpan.textContent = inputValues[i] || '_________' + ' ';
       } else wordSpan.textContent = wordObj.word + ' ';
-      // Append the word span to the preview view
       madLibsPreview.appendChild(wordSpan);
-      madLibsPreview.appendChild(document.createTextNode(" ")); // Add space
+      madLibsPreview.appendChild(document.createTextNode(" "));
     }
   }
 
-  // Initial rendering
   renderStory();
+
+  // Reset button event listener
+  const resetButton = document.querySelector('#resetButton');
+  resetButton.addEventListener('click', () => {
+    inputValues.fill('');
+    renderStory();
+  });
 });
+
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector('.madLibsEdit'); // Select the container div
+  const container = document.querySelector('.madLibsEdit');
 
   container.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
@@ -158,13 +144,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const currentIndex = Array.from(inputFields).indexOf(currentInput);
 
       if (currentIndex > -1) {
-        event.preventDefault(); // Prevent default form submission behavior
+        event.preventDefault();
         const nextIndex = currentIndex + 1;
 
         if (nextIndex < inputFields.length) {
-          inputFields[nextIndex].focus(); // Move focus to the next input field
+          inputFields[nextIndex].focus();
         } else {
-          inputFields[0].focus(); // If at the last input, move focus to the first input
+          inputFields[0].focus();
         }
       }
     }
